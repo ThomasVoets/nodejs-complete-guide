127b167d0b6046fd0390ad4781378ec083c7a8b1
Thomas Voets <voets_thomas@hotmail.com>
2020-07-01 09:39:32 +0200

    Blocking and Non-Blocking Code

--- 01 - Understanding the Basics/app.js
+++ 01 - Understanding the Basics/app.js
@@ -18,7 +18,7 @@ const server = http.createServer((req, res) => {
   if (url === '/message' && method === 'POST') {
     const body = [];
 
-    req.on('data', (chunk) => {
+    req.on('data', chunk => {
       console.log(chunk);
       body.push(chunk);
     });
@@ -27,11 +27,11 @@ const server = http.createServer((req, res) => {
       const parsedBody = Buffer.concat(body).toString();
       const message = parsedBody.split('=')[1];
 
-      fs.writeFileSync('message.txt', message);
-
-      res.statusCode = 302;
-      res.setHeader('Location', '/');
-      return res.end();
+      fs.writeFile('message.txt', message, err => {
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        return res.end();
+      });
     });
   }
 
