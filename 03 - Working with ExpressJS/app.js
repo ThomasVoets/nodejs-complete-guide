4c65f8026673520ee98df8094e08f1937bba0524
Thomas Voets <voets_thomas@hotmail.com>
2020-07-03 12:00:26 +0200

    Using Express Router

--- 03 - Working with ExpressJS/app.js
+++ 03 - Working with ExpressJS/app.js
@@ -1,27 +1,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+const shopRoutes = require('./routes/shop');
+const adminRoutes = require('./routes/admin');
+
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/', (req, res, next) => {
-  next();
-});
-
-app.use('/add-product', (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
-  );
-});
-
-app.post('/product', (req, res, next) => {
-  console.log(req.body);
-  res.redirect('/');
-});
-
-app.use('/', (req, res, next) => {
-  res.send('<h1>Hellow form Express.js</h1>');
-});
+app.use(shopRoutes);
+app.use(adminRoutes);
 
 app.listen(3000);
