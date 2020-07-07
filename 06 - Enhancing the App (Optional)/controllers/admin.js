fb81644d5de0d87141a05a48c5e6d5c3f7a42f2a
Thomas Voets <voets_thomas@hotmail.com>
2020-07-07 10:32:00 +0200

    Storing Product Data

--- 06 - Enhancing the App (Optional)/controllers/admin.js
+++ 06 - Enhancing the App (Optional)/controllers/admin.js
@@ -19,8 +19,11 @@ exports.getAddProduct = (req, res, next) => {
 
 exports.postAddProduct = (req, res, next) => {
   const title = req.body.title;
+  const imageUrl = req.body.imageUrl;
+  const price = req.body.price;
+  const description = req.body.description;
 
-  const product = new Product(title);
+  const product = new Product(title, imageUrl, price, description);
   product.save();
 
   res.redirect('/');
