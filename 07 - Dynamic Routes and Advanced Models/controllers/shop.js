6ea513a6dab5a055b608933625bab6c40ded548d
Thomas Voets <voets_thomas@hotmail.com>
2020-07-07 16:04:09 +0200

    Start of Section 'Dynamic Routes & Advanced Models'

---
+++ 07 - Dynamic Routes and Advanced Models/controllers/shop.js
@@ -0,0 +1,42 @@
+const Product = require('../models/product');
+
+exports.getIndex = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render('shop/index', {
+      pageTitle: 'Shop',
+      path: '/',
+      products: products,
+    });
+  });
+};
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll(products => {
+    res.render('shop/product-list', {
+      pageTitle: 'All Products',
+      path: '/products',
+      products: products,
+    });
+  });
+};
+
+exports.getCart = (req, res, next) => {
+  res.render('shop/cart', {
+    pageTitle: 'Your Cart',
+    path: '/cart',
+  });
+};
+
+exports.getOrders = (req, res, next) => {
+  res.render('shop/orders', {
+    pageTitle: 'Orders',
+    path: '/orders',
+  });
+};
+
+exports.getCheckout = (req, res, next) => {
+  res.render('shop/checkout', {
+    pageTitle: 'Checkout',
+    path: '/checkout',
+  });
+};
