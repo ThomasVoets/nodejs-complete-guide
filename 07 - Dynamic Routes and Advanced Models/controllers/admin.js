2b1535855a2f87d5959bca959c60c097a29ee751
Thomas Voets <voets_thomas@hotmail.com>
2020-07-08 10:51:53 +0200

    Pre-Populating the Edit Product Page with Data

--- 07 - Dynamic Routes and Advanced Models/controllers/admin.js
+++ 07 - Dynamic Routes and Advanced Models/controllers/admin.js
@@ -14,6 +14,7 @@ exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
+    editing: false,
   });
 };
 
@@ -31,14 +32,20 @@ exports.postAddProduct = (req, res, next) => {
 
 exports.getEditProduct = (req, res, next) => {
   const editMode = req.query.edit;
+  const productId = req.params.productId;
 
   if (!editMode) {
     return res.redirect('/');
   }
 
-  res.render('admin/edit-product', {
-    pageTitle: 'Edit Product',
-    path: '/admin/edit-product',
-    editing: editMode,
+  Product.findById(productId, product => {
+    if (!product) res.redirect('/');
+
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing: editMode,
+      product: product,
+    });
   });
 };
