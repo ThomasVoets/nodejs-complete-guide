f5ed7773eee97b9bb25057b2b32b6a0ee55965cd
Thomas Voets <voets_thomas@hotmail.com>
2020-07-08 10:00:34 +0200

    Adding a Cart Model

--- 07 - Dynamic Routes and Advanced Models/controllers/shop.js
+++ 07 - Dynamic Routes and Advanced Models/controllers/shop.js
@@ -1,4 +1,5 @@
 const Product = require('../models/product');
+const Cart = require('../models/cart');
 
 exports.getIndex = (req, res, next) => {
   Product.fetchAll(products => {
@@ -41,8 +42,11 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
   const productId = req.body.productId;
-  console.log(productId);
-  res.redirect('/cart');
+
+  Product.findById(productId, product => {
+    Cart.addProduct(productId, product.price);
+    res.redirect('/cart');
+  });
 };
 
 exports.getOrders = (req, res, next) => {
---
+++ 07 - Dynamic Routes and Advanced Models/models/cart.js
@@ -0,0 +1,44 @@
+const fs = require('fs');
+const path = require('path');
+
+const rootDir = require('../util/path');
+
+const filePath = path.join(rootDir, 'data', 'cart.json');
+
+module.exports = class Cart {
+  static addProduct(id, price) {
+    // Fetch the previous cart
+    fs.readFile(filePath, (err, fileContent) => {
+      let cart = { products: [], totalPrice: 0 };
+
+      if (!err) {
+        cart = JSON.parse(fileContent);
+      }
+
+      // Analyze the cart => Find existing product
+      const existingProductIndex = cart.products.findIndex(
+        prod => prod.id === id
+      );
+      const existingProduct = cart.products[existingProductIndex];
+      let updatedProduct;
+
+      // Add new product/ increase quantity
+      if (existingProduct) {
+        updatedProduct = { ...existingProduct };
+        updatedProduct.qty = updatedProduct.qty + 1;
+        cart.products = [...cart.products];
+        cart.products[existingProductIndex] = updatedProduct;
+      } else {
+        updatedProduct = { id: id, qty: 1 };
+        cart.products = [...cart.products, updatedProduct];
+      }
+
+      cart.totalPrice = cart.totalPrice + +price;
+
+      // Save the cart back to the file
+      fs.writeFile(filePath, JSON.stringify(cart), err => {
+        console.log(err);
+      });
+    });
+  }
+};
