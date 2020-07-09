47ac98819a46c4e344c6292ada87a906bee4d105
Thomas Voets <voets_thomas@hotmail.com>
2020-07-09 08:26:28 +0200

    Basic SQL & Creating a Table

--- 08 - SQL Introduction/app.js
+++ 08 - SQL Introduction/app.js
@@ -3,13 +3,20 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
+// Set environment variables
+const dotenv = require('dotenv');
+dotenv.config();
+
 const errorController = require('./controllers/error');
+
+const db = require('./util/database');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
 
+db.execute('SELECT * FROM products').then().catch();
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
