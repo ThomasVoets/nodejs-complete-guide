e939c17f424e9a450485492ccf50f5a41c76b5f2
Thomas Voets <voets_thomas@hotmail.com>
2020-07-09 09:20:12 +0200

    Connecting to the Database

--- 09 - Understanding Sequelize/util/database.js
+++ 09 - Understanding Sequelize/util/database.js
@@ -1,10 +1,10 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize').Sequelize;
 
-const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  database: 'node-complete',
-  password: process.env.DB_PASSWORD,
-});
+const sequelize = new Sequelize(
+  'node-complete',
+  'root',
+  process.env.DB_PASSWORD,
+  { dialect: 'mysql', host: 'localhost' }
+);
 
-module.exports = pool.promise();
+module.exports = sequelize;
