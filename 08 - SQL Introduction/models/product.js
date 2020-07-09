0740e812ecd18959bc08d3b32accc186595aa530
Thomas Voets <voets_thomas@hotmail.com>
2020-07-09 08:58:12 +0200

    Inserting Data Into the Database

--- 08 - SQL Introduction/models/product.js
+++ 08 - SQL Introduction/models/product.js
@@ -11,7 +11,12 @@ module.exports = class Product {
     this.description = description;
   }
 
-  save() {}
+  save() {
+    return db.execute(
+      'INSERT INTO products (title, imageUrl, price, description) VALUES (?, ?, ?, ?)',
+      [this.title, this.imageUrl, this.price, this.description]
+    );
+  }
 
   static fetchAll() {
     return db.execute('SELECT * FROM products');
