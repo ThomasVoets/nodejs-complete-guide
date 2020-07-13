a9c0046feeb8dcdb7b48c957b9130fb1478c41aa
Thomas Voets <voets_thomas@hotmail.com>
2020-07-13 11:25:26 +0200

    Storing Cartitems as Orderitems

--- 09 - Understanding Sequelize/controllers/shop.js
+++ 09 - Understanding Sequelize/controllers/shop.js
@@ -1,5 +1,4 @@
 const Product = require('../models/product');
-const Cart = require('../models/cart');
 
 exports.getIndex = (req, res, next) => {
   Product.findAll()
@@ -127,6 +126,34 @@ exports.getOrders = (req, res, next) => {
     pageTitle: 'Orders',
     path: '/orders',
   });
+};
+
+exports.postOrder = (req, res, next) => {
+  let cartProducts;
+
+  req.user
+    .getCart()
+    .then(cart => {
+      return cart.getProducts();
+    })
+    .then(products => {
+      cartProducts = products;
+      return req.user.createOrder();
+    })
+    .then(order => {
+      return order.addProducts(
+        cartProducts.map(product => {
+          product.orderItem = { quantity: product.cartItem.quantity };
+          return product;
+        })
+      );
+    })
+    .then(result => {
+      res.redirect('/orders');
+    })
+    .catch(err => {
+      console.log(err);
+    });
 };
 
 exports.getCheckout = (req, res, next) => {
