98ffc6ebbd032aac667ef4daed8eab54924f26bc
Thomas Voets <voets_thomas@hotmail.com>
2020-07-13 09:11:16 +0200

    Adding a One-To-Many Relationship

--- 09 - Understanding Sequelize/app.js
+++ 09 - Understanding Sequelize/app.js
@@ -11,6 +11,9 @@ const errorController = require('./controllers/error');
 
 const sequelize = require('./util/database');
 
+const Product = require('./models/product');
+const User = require('./models/user');
+
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
@@ -27,8 +30,11 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
+Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
+User.hasMany(Product); // This is optional
+
 sequelize
-  .sync()
+  .sync({ force: true })
   .then(result => {
     app.listen(3000);
   })
