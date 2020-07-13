78b3e0dd0180f5ddb5617a70a53cbfbf06860744
Thomas Voets <voets_thomas@hotmail.com>
2020-07-13 10:34:52 +0200

    Adding New Products to the Cart

--- 09 - Understanding Sequelize/controllers/shop.js
+++ 09 - Understanding Sequelize/controllers/shop.js
@@ -65,11 +65,42 @@ exports.getCart = (req, res, next) => {
 
 exports.postCart = (req, res, next) => {
   const productId = req.body.productId;
+  let fetchedCart;
 
-  Product.findById(productId, product => {
-    Cart.addProduct(productId, product.price);
-    res.redirect('/cart');
-  });
+  req.user
+    .getCart()
+    .then(cart => {
+      fetchedCart = cart;
+      return cart.getProducts({ where: { id: productId } });
+    })
+    .then(products => {
+      let product;
+      let newQuantity = 1;
+
+      if (products.length > 0) {
+        product = products[0];
+      }
+
+      if (product) {
+        // ...
+      }
+
+      return Product.findByPk(productId)
+        .then(product => {
+          return fetchedCart.addProduct(product, {
+            through: { quantity: newQuantity },
+          });
+        })
+        .catch(err => {
+          console.log(err);
+        });
+    })
+    .then(result => {
+      res.redirect('/cart');
+    })
+    .catch(err => {
+      console.log(err);
+    });
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
