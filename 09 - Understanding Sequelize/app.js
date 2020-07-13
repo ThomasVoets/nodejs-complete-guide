a73c04633dd392169b92dac30c88c4ddf762cba8
Thomas Voets <voets_thomas@hotmail.com>
2020-07-13 09:57:04 +0200

    One-To-Many & Many-To-Many Relations

--- 09 - Understanding Sequelize/app.js
+++ 09 - Understanding Sequelize/app.js
@@ -13,6 +13,8 @@ const sequelize = require('./util/database');
 
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
@@ -45,8 +47,14 @@ app.use(errorController.get404);
 Product.belongsTo(User, { constraints: true, onDelete: 'CASCADE' });
 User.hasMany(Product); // This is optional
 
+User.hasOne(Cart);
+Cart.belongsTo(User);
+
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
+
 sequelize
-  .sync()
+  .sync({ force: true })
   .then(result => {
     return User.findByPk(1);
   })
