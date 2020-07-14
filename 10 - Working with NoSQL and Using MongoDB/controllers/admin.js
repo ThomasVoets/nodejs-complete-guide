ad49c91afafddfc9d442074de5bc334cd3406c35
Thomas Voets <voets_thomas@hotmail.com>
2020-07-14 08:34:22 +0200

    Creating Products

--- 10 - Working with NoSQL and Using MongoDB/controllers/admin.js
+++ 10 - Working with NoSQL and Using MongoDB/controllers/admin.js
@@ -29,13 +29,10 @@ exports.postAddProduct = (req, res, next) => {
   const price = req.body.price;
   const description = req.body.description;
 
-  req.user
-    .createProduct({
-      title: title,
-      imageUrl: imageUrl,
-      price: price,
-      description: description,
-    })
+  const product = new Product(title, imageUrl, price, description);
+
+  product
+    .save()
     .then(result => {
       console.log('Created Product');
       res.redirect('/admin/products');
