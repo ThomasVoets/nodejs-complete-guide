bffe9c5dd11e260812f88b335f0a5c68ceb5fe58
Thomas Voets <voets_thomas@hotmail.com>
2020-07-16 08:29:25 +0200

    Connecting to the MongoDB Server with Mongoose

--- 11 - Working with Mongoose/app.js
+++ 11 - Working with Mongoose/app.js
@@ -2,19 +2,18 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
-
-const mongoConnect = require('./util/database').mongoConnect;
+const mongoose = require('mongoose');
 
 // Set environment variables
 const dotenv = require('dotenv');
 dotenv.config();
 
-const User = require('./models/user');
+// const User = require('./models/user');
 
 const errorController = require('./controllers/error');
 
-const adminRoutes = require('./routes/admin');
-const shopRoutes = require('./routes/shop');
+// const adminRoutes = require('./routes/admin');
+// const shopRoutes = require('./routes/shop');
 
 const app = express();
 
@@ -26,21 +25,23 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Add a 'Dummy User' to the request
 app.use((req, res, next) => {
-  User.findById('5f0d99e20fe8012944919c68')
-    .then(user => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch(err => {
-      console.log(err);
-    });
+  next();
 });
 
-app.use('/admin', adminRoutes);
-app.use(shopRoutes);
+// app.use('/admin', adminRoutes);
+// app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  app.listen(3000);
-});
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(result => {
+    console.log('Connected to MongoDB via Mongoose');
+    app.listen(3000);
+  })
+  .catch(err => {
+    console.log(err);
+  });
