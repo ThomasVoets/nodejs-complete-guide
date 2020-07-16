9d85ab4d47dfcacb38c56cecb4f8fa545156f3f1
Thomas Voets <voets_thomas@hotmail.com>
2020-07-16 08:58:35 +0200

    Deleting Products

--- 11 - Working with Mongoose/controllers/admin.js
+++ 11 - Working with Mongoose/controllers/admin.js
@@ -95,7 +95,7 @@ exports.postEditProduct = (req, res, next) => {
 exports.postDeleteProduct = (req, res, next) => {
   const productId = req.body.productId;
 
-  Product.deleteById(productId)
+  Product.findByIdAndRemove(productId)
     .then(() => {
       console.log('Destroyed Product');
       res.redirect('/admin/products');
