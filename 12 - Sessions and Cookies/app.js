9b746ae71d96e9cd2482e5a4326dbbf4f38eed4c
Thomas Voets <voets_thomas@hotmail.com>
2020-07-24 12:27:05 +0200

    Start of Section 'Sessions & Cookies'

---
+++ 12 - Sessions and Cookies/app.js
@@ -0,0 +1,65 @@
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+// Set environment variables
+const dotenv = require('dotenv');
+dotenv.config();
+
+const User = require('./models/user');
+
+const errorController = require('./controllers/error');
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Add a 'Dummy User' to the request
+app.use((req, res, next) => {
+  User.findById('5f10586c99f65259d42c98a7')
+    .then(user => {
+      req.user = user;
+      next();
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+mongoose
+  .connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(result => {
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'Thomas',
+          email: 'thomas@test.com',
+          cart: { items: [] },
+        });
+        user.save();
+      }
+    });
+
+    app.listen(3000);
+    console.log('Connected to MongoDB via Mongoose');
+  })
+  .catch(err => {
+    console.log(err);
+  });
---
+++ 12 - Sessions and Cookies/controllers/shop.js
@@ -0,0 +1,141 @@
+const Product = require('../models/product');
+const Order = require('../models/order');
+
+exports.getIndex = (req, res, next) => {
+  Product.find()
+    .then(products => {
+      res.render('shop/index', {
+        pageTitle: 'Shop',
+        path: '/',
+        products: products,
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
+
+exports.getProducts = (req, res, next) => {
+  Product.find()
+    .then(products => {
+      res.render('shop/product-list', {
+        pageTitle: 'All Products',
+        path: '/products',
+        products: products,
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
+
+exports.getProduct = (req, res, next) => {
+  const productId = req.params.productId;
+
+  Product.findById(productId)
+    .then(product => {
+      res.render('shop/product-detail', {
+        pageTitle: product.title,
+        path: '/products',
+        product: product,
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
+
+exports.getCart = (req, res, next) => {
+  req.user
+    .populate('cart.items.productId')
+    .execPopulate()
+    .then(user => {
+      const products = user.cart.items;
+      res.render('shop/cart', {
+        pageTitle: 'Your Cart',
+        path: '/cart',
+        products: products,
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
+
+exports.postCart = (req, res, next) => {
+  const productId = req.body.productId;
+
+  Product.findById(productId)
+    .then(product => {
+      return req.user.addToCart(product);
+    })
+    .then(result => {
+      res.redirect('/cart');
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
+
+exports.postCartDeleteProduct = (req, res, next) => {
+  const productId = req.body.productId;
+
+  req.user
+    .removeFromCart(productId)
+    .then(result => {
+      res.redirect('/cart');
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
+
+exports.getOrders = (req, res, next) => {
+  Order.find({ 'user.userId': req.user._id })
+    .populate('user.userId', 'name email')
+    .exec()
+    .then(orders => {
+      res.render('shop/orders', {
+        pageTitle: 'Orders',
+        path: '/orders',
+        orders: orders,
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
+
+exports.postOrder = (req, res, next) => {
+  req.user
+    .populate('cart.items.productId')
+    .execPopulate()
+    .then(user => {
+      const products = user.cart.items.map(i => {
+        return { product: { ...i.productId._doc }, quantity: i.quantity };
+      });
+
+      const order = new Order({
+        user: { userId: req.user },
+        products: products,
+      });
+
+      return order.save();
+    })
+    .then(result => {
+      return req.user.clearCart();
+    })
+    .then(() => {
+      res.redirect('/orders');
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
+
+exports.getCheckout = (req, res, next) => {
+  res.render('shop/checkout', {
+    pageTitle: 'Checkout',
+    path: '/checkout',
+  });
+};
