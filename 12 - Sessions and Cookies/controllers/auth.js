38fe117facdd5428ba45c73b0ef692e3680d0717
Thomas Voets <voets_thomas@hotmail.com>
2020-07-24 12:59:06 +0200

    Setting a Cookie

--- 12 - Sessions and Cookies/controllers/auth.js
+++ 12 - Sessions and Cookies/controllers/auth.js
@@ -1,12 +1,14 @@
 exports.getLogin = (req, res, next) => {
+  const isLoggedIn = req.get('Cookie').split('=')[1];
+
   res.render('auth/login', {
     pageTitle: 'Login',
     path: '/login',
-    isAuthenticated: req.isLoggedIn,
+    isAuthenticated: isLoggedIn,
   });
 };
 
 exports.postLogin = (req, res, next) => {
-  req.isLoggedIn = true;
+  res.setHeader('Set-Cookie', 'loggedIn=true');
   res.redirect('/');
 };
