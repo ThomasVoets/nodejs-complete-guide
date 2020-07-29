c5ab2bd49e7be4beddafbe9c7d34119847e7831b
Thomas Voets <voets_thomas@hotmail.com>
2020-07-29 08:29:46 +0200

    Creating the Reset Password Form

--- 15 - Advanced Authentication/controllers/auth.js
+++ 15 - Advanced Authentication/controllers/auth.js
@@ -192,3 +192,27 @@ exports.postReset = (req, res, next) => {
       });
   });
 };
+
+exports.getNewPassword = (req, res, next) => {
+  const token = req.params.token;
+  let message = req.flash('error');
+
+  if (message.length > 0) {
+    message = message[0];
+  } else {
+    message = null;
+  }
+
+  User.findOne({ resetToken: token, resetTokenExpiration: { $gt: Date.now() } })
+    .then(user => {
+      res.render('auth/new-password', {
+        path: '/new-password',
+        pageTitle: 'New Password',
+        errorMessage: message,
+        userId: user._id.toString(),
+      });
+    })
+    .catch(err => {
+      console.log(err);
+    });
+};
