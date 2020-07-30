9900c4e2c9547f2250d26c08d84d6a70fb3574a0
Thomas Voets <voets_thomas@hotmail.com>
2020-07-30 08:41:13 +0200

    More Validators

--- 16 - Understanding Validation/routes/auth.js
+++ 16 - Understanding Validation/routes/auth.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { check } = require('express-validator');
+const { check, body } = require('express-validator');
 
 const authController = require('../controllers/auth');
 
@@ -13,14 +13,22 @@ router.get('/signup', authController.getSignup);
 
 router.post(
   '/signup',
-  check('email')
-    .isEmail()
-    .withMessage('Please enter a valid email')
-    .custom((value, { req }) => {
-      if (value === 'test@test.com')
-        throw new Error('This email address is forbidden');
-      return true;
-    }),
+  [
+    check('email')
+      .isEmail()
+      .withMessage('Please enter a valid email')
+      .custom((value, { req }) => {
+        if (value === 'test@test.com')
+          throw new Error('This email address is forbidden');
+        return true;
+      }),
+    body(
+      'password',
+      'Please enter a 5 character long password with only numbers and text'
+    )
+      .isLength({ min: 5 })
+      .isAlphanumeric(),
+  ],
   authController.postSignup
 );
 
