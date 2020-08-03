d87e5e72007e1f1450b715afefeb253202047464
Thomas Voets <voets_thomas@hotmail.com>
2020-08-03 09:07:01 +0200

    Downloading Files with Authentication

--- 18 - File Upload and Download/controllers/shop.js
+++ 18 - File Upload and Download/controllers/shop.js
@@ -1,3 +1,6 @@
+const fs = require('fs');
+const path = require('path');
+
 const Product = require('../models/product');
 const Order = require('../models/order');
 
@@ -147,6 +150,21 @@ exports.postOrder = (req, res, next) => {
       error.httpStatusCode = 500;
       return next(error);
     });
+};
+
+exports.getInvoice = (req, res, next) => {
+  const orderId = req.params.orderId;
+
+  const invoiceName = 'invoice-' + orderId + '.pdf';
+  const invoicePath = path.join('data', 'invoices', invoiceName);
+
+  fs.readFile(invoicePath, (err, data) => {
+    if (err) {
+      return next(err);
+    }
+
+    res.send(data);
+  });
 };
 
 exports.getCheckout = (req, res, next) => {
