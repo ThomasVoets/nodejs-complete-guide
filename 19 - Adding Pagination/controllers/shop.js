9738a8b2c6de3d085f241902b284f739941f7104
Thomas Voets <voets_thomas@hotmail.com>
2020-10-08 13:04:48 +0200

    Re-Using the Pagination Logic & Controls

--- 19 - Adding Pagination/controllers/shop.js
+++ 19 - Adding Pagination/controllers/shop.js
@@ -44,12 +44,31 @@ exports.getIndex = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
+  const page = +req.query.page || 1;
+
+  let totalItems;
+
   Product.find()
+    .countDocuments()
+    .then(numProducts => {
+      totalItems = numProducts;
+
+      return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+    })
     .then(products => {
       res.render('shop/product-list', {
         pageTitle: 'All Products',
         path: '/products',
         products: products,
+        currentPage: page,
+        totalProducts: totalItems,
+        hasNextPage: ITEMS_PER_PAGE * page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE),
       });
     })
     .catch(err => {
