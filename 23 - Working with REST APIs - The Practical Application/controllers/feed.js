c25774ae29c05ea5972e5444816f2cd153f3d936
Thomas Voets <voets_thomas@hotmail.com>
2020-10-26 19:30:05 +0100

    Fetching Lists of Posts

--- 23 - Working with REST APIs - The Practical Application/controllers/feed.js
+++ 23 - Working with REST APIs - The Practical Application/controllers/feed.js
@@ -1,6 +1,17 @@
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
-    posts: [{ title: 'First Post', content: 'This is the first post!' }],
+    posts: [
+      {
+        _id: '1',
+        title: 'First Post',
+        content: 'This is the first post!',
+        imageUrl: 'images/duck.png',
+        creator: {
+          name: 'Thomas',
+        },
+        createdAt: new Date(),
+      },
+    ],
   });
 };
 
@@ -10,10 +21,8 @@ exports.postPost = (req, res, next) => {
 
   // create post in db ...
 
-  res
-    .status(201)
-    .json({
-      message: 'Post created successfully',
-      post: { id: new Date().toISOString(), title: title, content: content },
-    });
+  res.status(201).json({
+    message: 'Post created successfully',
+    post: { id: new Date().toISOString(), title: title, content: content },
+  });
 };
