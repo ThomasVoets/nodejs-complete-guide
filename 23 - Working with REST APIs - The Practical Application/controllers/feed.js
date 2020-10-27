d559c643027108e7c2bb52d6eec5f83b7c6cd04d
Thomas Voets <voets_thomas@hotmail.com>
2020-10-27 08:12:23 +0100

    Storing Posts in the Database

--- 23 - Working with REST APIs - The Practical Application/controllers/feed.js
+++ 23 - Working with REST APIs - The Practical Application/controllers/feed.js
@@ -1,5 +1,7 @@
 const { validationResult } = require('express-validator/check');
 
+const Post = require('../models/post');
+
 exports.getPosts = (req, res, next) => {
   res.status(200).json({
     posts: [
@@ -30,16 +32,21 @@ exports.postPost = (req, res, next) => {
   const title = req.body.title;
   const content = req.body.content;
 
-  // create post in db ...
-
-  res.status(201).json({
-    message: 'Post created successfully',
-    post: {
-      _id: new Date().toISOString(),
-      title: title,
-      content: content,
-      creator: { name: 'Thomas' },
-      createdAt: new Date(),
-    },
+  const post = new Post({
+    title: title,
+    content: content,
+    imageUrl: 'images/duck.png',
+    creator: { name: 'Thomas' },
   });
+
+  post
+    .save()
+    .then(result => {
+      console.log(result);
+      res.status(201).json({
+        message: 'Post created successfully',
+        post: result,
+      });
+    })
+    .catch(err => console.log(err));
 };
