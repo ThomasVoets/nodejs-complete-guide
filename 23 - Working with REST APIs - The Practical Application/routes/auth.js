410c11e7eed983e03ab4f142ddc66f03684a976f
Thomas Voets <voets_thomas@hotmail.com>
2020-10-30 13:03:34 +0100

    Time to Practice - Working with REST APIs

--- 23 - Working with REST APIs - The Practical Application/routes/auth.js
+++ 23 - Working with REST APIs - The Practical Application/routes/auth.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const { body } = require('express-validator/check');
 
+const isAuth = require('../middleware/isAuth');
+
 const User = require('../models/user');
 
 const authController = require('../controllers/auth');
@@ -28,5 +30,14 @@ router.put(
 );
 
 router.post('/login', authController.login);
+
+router.get('/status', isAuth, authController.getUserStatus);
+
+router.put(
+  '/status',
+  isAuth,
+  [body('status').trim().not().isEmpty()],
+  authController.updateUserStatus
+);
 
 module.exports = router;
