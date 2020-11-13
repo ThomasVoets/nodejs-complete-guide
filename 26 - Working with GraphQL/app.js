0d12e6b9ff1db707833bc601e853917f2d9f0fe5
Thomas Voets <voets_thomas@hotmail.com>
2020-11-13 13:37:53 +0100

    Viewing a Single Post

--- 26 - Working with GraphQL/app.js
+++ 26 - Working with GraphQL/app.js
@@ -60,6 +60,10 @@ app.use((req, res, next) => {
 app.use(auth);
 
 app.put('/post-image', (req, res, next) => {
+  if (!req.isAuth) {
+    throw new Error('Not authenticated!');
+  }
+
   if (!req.file) {
     return res.status(200).json({ message: 'No file provided' });
   }
@@ -70,7 +74,10 @@ app.put('/post-image', (req, res, next) => {
 
   return res
     .status(201)
-    .json({ message: 'File stored', filePath: req.filePath });
+    .json({
+      message: 'File stored',
+      filePath: req.file.path.replace('\\', '/'),
+    });
 });
 
 app.use(
