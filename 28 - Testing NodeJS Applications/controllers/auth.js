28d9c7e18fe8473db3a714421173366c77423f7e
Thomas Voets <voets_thomas@hotmail.com>
2020-11-18 08:43:47 +0100

    Testing Asynchronous Code

--- 28 - Testing NodeJS Applications/controllers/auth.js
+++ 28 - Testing NodeJS Applications/controllers/auth.js
@@ -69,11 +69,13 @@ exports.login = async (req, res, next) => {
     );
 
     res.status(200).json({ token: token, userId: user._id.toString() });
+    return;
   } catch (err) {
     if (!err.statusCode) {
       err.statusCode = 500;
     }
     next(err);
+    return err;
   }
 };
 
--- 28 - Testing NodeJS Applications/test/auth-controller.js
+++ 28 - Testing NodeJS Applications/test/auth-controller.js
@@ -7,11 +7,22 @@ const User = require('../models/user');
 
 describe('Auth Controller', function () {
   describe('Login process', function () {
-    it('should throw an error if accessing the database failsn', function () {
+    it('should throw an error if accessing the database fails', function (done) {
       sinon.stub(User, 'findOne');
       User.findOne.throws();
 
-      expect(AuthController.login);
+      const req = {
+        body: {
+          email: 'test@test.com',
+          password: 'tester',
+        },
+      };
+
+      AuthController.login(req, {}, () => {}).then(result => {
+        expect(result).to.be.an('error');
+        expect(result).to.have.property('statusCode', 500);
+        done();
+      });
 
       User.findOne.restore();
     });
