86712b5d2cdf8ee266b6a52288e102e30ccd20ef
Thomas Voets <voets_thomas@hotmail.com>
2020-11-18 13:18:50 +0100

    Node Core Modules & Promises

--- 29 - Modern JavaScript & NodeJS/response-handler.js
+++ 29 - Modern JavaScript & NodeJS/response-handler.js
@@ -1,11 +1,12 @@
-import path, { dirname } from 'path';
-import { fileURLToPath } from 'url';
+import fs from 'fs/promises';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const resHandler = (req, res, next) => {
-  res.sendFile(path.join(__dirname, 'my-page.html'));
+const resHandler = async (req, res, next) => {
+  try {
+    const data = await fs.readFile('my-page.html', 'utf8');
+    res.send(data);
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 export default resHandler;
