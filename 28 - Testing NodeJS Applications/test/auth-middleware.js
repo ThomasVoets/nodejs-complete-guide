d0b3fac85867a20538441efdc5598ecb829b4eee
Thomas Voets <voets_thomas@hotmail.com>
2020-11-18 07:53:55 +0100

    What Not To Test!

--- 28 - Testing NodeJS Applications/test/auth-middleware.js
+++ 28 - Testing NodeJS Applications/test/auth-middleware.js
@@ -28,4 +28,29 @@ describe('Auth middleware', function () {
 
     expect(authMiddleware.bind(this, req, res, next)).to.throw();
   });
+
+  it('should throw an error if the token cannot be verified', function () {
+    const req = {
+      get: function (headerName) {
+        return 'Bearer xyz';
+      },
+    };
+    const res = {};
+    const next = () => {};
+
+    expect(authMiddleware.bind(this, req, res, next)).to.throw();
+  });
+
+  it('should yield a userId after decoding the token', function () {
+    const req = {
+      get: function (headerName) {
+        return 'Bearer fdfsfgdsgsgsdfdsf';
+      },
+    };
+    const res = {};
+    const next = () => {};
+
+    authMiddleware(req, res, next);
+    expect(req).to.have.property('userId');
+  });
 });
