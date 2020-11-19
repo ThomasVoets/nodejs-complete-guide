c45bffad461c11234d0ecfc8df8ede02a357e0ff
Thomas Voets <voets_thomas@hotmail.com>
2020-11-19 11:12:55 +0100

    Working with Type Aliases & Interfaces

--- 30 - NodeJS and TypeScript/app.ts
+++ 30 - NodeJS and TypeScript/app.ts
@@ -5,7 +5,15 @@ const buttonElement = document.querySelector('button')!;
 const numResults: number[] = [];
 const textResults: string[] = [];
 
-function add(num1: number | string, num2: number | string) {
+type NumOrString = number | string;
+type Result = { val: number; timeStap: Date };
+
+interface ResultObj {
+  val: number;
+  timeStap: Date;
+}
+
+function add(num1: NumOrString, num2: NumOrString) {
   if (typeof num1 === 'number' && typeof num2 === 'number') {
     return num1 + num2;
   } else if (typeof num1 === 'string' && typeof num2 === 'string') {
@@ -15,7 +23,7 @@ function add(num1: number | string, num2: number | string) {
   }
 }
 
-function printResult(resultObj: { val: number; timeStap: Date }) {
+function printResult(resultObj: ResultObj) {
   console.log(resultObj.val);
 }
 
