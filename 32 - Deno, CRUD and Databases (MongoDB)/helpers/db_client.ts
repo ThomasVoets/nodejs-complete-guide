5686329bfddba54801a592fd771a6b7fb8046194
Thomas Voets <voets_thomas@hotmail.com>
2020-11-20 10:49:49 +0100

    Using the MongoDB Client Module

--- 32 - Deno, CRUD and Databases (MongoDB)/helpers/db_client.ts
+++ 32 - Deno, CRUD and Databases (MongoDB)/helpers/db_client.ts
@@ -7,11 +7,7 @@ let db: Database;
 
 export function connect() {
   const client = new MongoClient();
-  client.connectWithUri(
-    `mongodb+srv://${Deno.env.get('DB_USER')}:${Deno.env.get(
-      'DB_PASSWORD'
-    )}@denotodosdb.nwrjr.mongodb.net/?retryWrites=true&w=majority`
-  );
+  client.connectWithUri('mongodb://localhost:27017');
 
   db = client.database('deno-todo-app');
 }
--- 32 - Deno, CRUD and Databases (MongoDB)/routes/todos.ts
+++ 32 - Deno, CRUD and Databases (MongoDB)/routes/todos.ts
@@ -1,29 +1,42 @@
 import { Router } from 'https://deno.land/x/oak/mod.ts';
+import { ObjectId } from 'https://deno.land/x/mongo@v0.13.0/mod.ts';
 
 import { getDb } from '../helpers/db_client.ts';
 
 const router = new Router();
 
 interface Todo {
-  id: string;
+  id?: string;
+  text: string;
+}
+
+interface TodoSchema {
+  _id: { $oid: string };
   text: string;
 }
 
 let todos: Todo[] = [];
 
-router.get('/todos', ctx => {
-  ctx.response.body = { todos: todos };
+router.get('/todos', async ctx => {
+  const todos = await getDb().collection<TodoSchema>('todos').find(); // {_id: ObjectID(), text: '...'}
+  const transformedTodos = todos.map(
+    (todo: { _id: ObjectId; text: string }) => {
+      return { id: todo._id.$oid, text: todo.text };
+    }
+  );
+
+  ctx.response.body = { todos: transformedTodos };
 });
 
 router.post('/todos', async (ctx: any) => {
-  const data = await ctx.request.body().value;
+  const result = await ctx.request.body();
+  const data = await result.value;
 
-  const newTodo: Todo = {
-    id: new Date().toISOString(),
-    text: data.text,
-  };
+  const newTodo: Todo = { text: data.text };
 
-  todos.push(newTodo);
+  const id = await getDb().collection<TodoSchema>('todos').insertOne(newTodo);
+
+  newTodo.id = id.$oid;
 
   ctx.response.body = { message: 'Created todo!', todo: newTodo };
 });
